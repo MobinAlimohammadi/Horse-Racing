239c5b92e84cca7cc830d134288cc28ee186bc7e
Mobin Alimohammadi <malimohammadi@bayviewglen.ca>
2024-01-18 10:35:49 -0500

    player updates - incomplete-

--- Horse Racing Assignment/src/horseracing/Player.java
+++ Horse Racing Assignment/src/horseracing/Player.java
@@ -20,7 +20,7 @@ public class Player{
         
        public String askName() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter your name: ");
+        System.out.print("Please enter your name: ");
         return in.nextLine();
        }
 
@@ -44,6 +44,16 @@ public class Player{
         currentBet = amount;
        }
 
+       public void printBetModes(){
+
+        System.out.println("Select your bet mode by selecting:");
+        System.out.println("1 - WIN");
+        System.out.println("2 - PLACE");
+        System.out.println("3 - SHOW");
+
+
+       }
+
        public void betMode(String mode){
         if (mode.equals("win")){
             win();
@@ -57,48 +67,59 @@ public class Player{
     
        private void win() {
         
-    }
+        }
 
 
 
-    private void place() {
+        private void place() {
         
-    }
+        }
 
 
 
-    private void show() {
+        private void show() {
        
-    }
+        }
+
+        public void displayPlayerInfo(){
+            for (int i = 0; i < 5; i++){
+                String s1 = "name: " + name;
+                String s2 = "ballance: " + wallet;
+                String s3 = "bet amount: " + currentBet; // make multiple betts and make the table dependent 
+                String s4 = "horse: " + horse;
+
+                System.out.println("+-----+-----+-----+-----+");
+                System.out.printf("|%5s|%5s|%5s|%5s|\n", s1,s2,s3,s4);
+            }
+            System.out.printf("+-----+-----+-----+-----+");
+        }
 
 
-
-    public void askBet(List<Horse> horses) {
-        Scanner in = new Scanner(System.in);
-        boolean validBet = false;
-        boolean validHorse = false;
-        double amount = 0;
-        System.out.println("Made it here");
-        while(!validBet) {
-            System.out.println("Wallet: " + getWallet());
-            System.out.println("Please enter bet amount: ");
-            amount = in.nextInt();
-            if (amount >= 2 && amount <= wallet) {
-                validBet = true;
-            } else {
-                if(amount < 2) {
-                System.out.println("$2 Minimum Bet");
+        public void askBet(List<Horse> horses) {
+            Scanner in = new Scanner(System.in);
+            boolean validBet = false;
+            boolean validHorse = false;
+            double amount = 0;
+            while(!validBet) {
+                System.out.println("Wallet: " + getWallet());
+                System.out.println("Please enter bet amount: ");
+                amount = in.nextDouble();
+                if (amount >= 2 && amount <= wallet) {
+                    validBet = true;
                 } else {
-                    System.out.println("Not Enough Funds");
+                    if(amount < 2) {
+                    System.out.println("$2 Minimum Bet");
+                    } else {
+                        System.out.println("Not Enough Funds");
+                    }
                 }
             }
-        }
-        
-        for (Horse horse : horses) {
+            
+            for (Horse horse : horses) {
 
-        }
-        setBet(amount);
-        setWallet((getWallet() - amount));
+            }
+            setBet(amount);
+            setWallet((getWallet() - amount));
        }
        
        
